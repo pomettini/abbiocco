0daf23d2d3c7c29296f2b1982473fb7f08377f62
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-02-09 23:27:39 +0100

    Mapped some functions

--- resources/main.lua
+++ resources/main.lua
@@ -1,14 +1,15 @@
--- function _init()
+cur_frame = 0
+
+function _init()
     
--- end
+end
 
--- function _update()
-	
--- end
+function _update()
+	cur_frame = cur_frame + 1
+end
 
--- function _draw()
-	
--- end
+function _draw()
+	rectfill(10, 10, 50, 50, 1)
+end
 
 -- spr(1, 60, 60)
-rectfill(10, 10, 50, 50, 1)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate hlua;
 extern crate sdl2;
 
-use hlua::Lua;
+use hlua::{AnyLuaValue, Lua};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -9,13 +9,12 @@ use sdl2::rect::{Point, Rect};
 use sdl2::*;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 use std::time::Duration;
 
-pub fn main() {
-    let mut file = File::open("resources/main.lua").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+static TARGET_FPS: u32 = 30;
 
+pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -34,10 +33,34 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut lua = Lua::new();
-    lua.execute::<()>(&contents).unwrap();
+
+    fn rectfill_func(x0: u32, y0: u32, x1: u32, y1: u32) {
+        // canvas.fill_rect(Rect::new(10, 10, 780, 580));
+        println!("{:?}", x0);
+    }
+
+    lua.set("rectfill", hlua::function4(rectfill_func));
+
+    lua.execute_from_reader::<(), _>(File::open(&Path::new("resources/main.lua")).unwrap())
+        .unwrap();
 
     'running: loop {
-        // Draw stuff
+        // Start draw stuff
+
+        {
+            let mut update_func: hlua::LuaFunction<_> = lua.get("_update").unwrap();
+            let update_ret: hlua::AnyLuaValue = update_func.call().unwrap();
+        }
+
+        {
+            let mut draw_func: hlua::LuaFunction<_> = lua.get("_draw").unwrap();
+            let draw_ret: hlua::AnyLuaValue = draw_func.call().unwrap();
+        }
+
+        let cur_frame: i32 = lua.get("cur_frame").unwrap();
+        println!("{:?}", cur_frame);
+
+        // End draw stuff
 
         for event in event_pump.poll_iter() {
             match event {
@@ -50,7 +73,7 @@ pub fn main() {
             }
         }
 
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        // canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / TARGET_FPS));
     }
 }
